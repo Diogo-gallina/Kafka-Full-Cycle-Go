f37af9273f7805bedf6df192ee31d35cbdf3d4d8
Diogo-gallina <diogogallina20@gmail.com>
2023-10-11 18:54:23 -0300

    feat: finish producer config

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -9,22 +9,18 @@ import (
 func main()  {
   deliveryChan := make(chan kafka.Event)
   producer := NewKafkaProducer()
-  Publish("mensagem", "teste", producer, nil, deliveryChan)
+  Publish("transferiu", "teste", producer, []byte("transferencia3"), deliveryChan)
+  go DeliveryReport(deliveryChan)
 
-  e :=  <-deliveryChan
-  msg := e.(*kafka.Message)
-  if msg.TopicPartition.Error != nil {
-    fmt.Println("Erro ao enviar")
-  } else { 
-    fmt.Println("Mensagem enviada: ", msg.TopicPartition)    
-  }
-
-  producer.Flush(1000)
+  producer.Flush(4000)
 }
 
 func NewKafkaProducer() *kafka.Producer {
   configMap := &kafka.ConfigMap{
 	"bootstrap.servers": "kafka-full-cycle-go_kafka_1:9092",
+  "delivery.timeout.ms": "0",
+  "acks": "all",
+  "enable.idempotence": "true",
   }
   p, err := kafka.NewProducer(configMap)
   if err != nil {
@@ -44,4 +40,17 @@ func Publish(msg string, topic string, producer *kafka.Producer, key []byte, del
 	return err
   }
   return nil
+}
+
+func DeliveryReport(deliveryChan chan kafka.Event) {
+  for e := range deliveryChan {
+    switch ev := e.(type) {
+    case *kafka.Message:
+      if ev.TopicPartition.Error != nil {
+        fmt.Println("Erro ao enviar")
+      } else { 
+        fmt.Println("Mensagem enviada: ", ev.TopicPartition)    
+      }
+    }
+  }
 }
